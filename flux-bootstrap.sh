371edf9dde5e51d180bc56865d00db116b136821
Carlos Marcelo Rodriguez de Luna <crodriguezde@simplefxn.com>
2021-12-10 21:11:40 -0600

    Change

--- flux-bootstrap.sh
+++ flux-bootstrap.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 kubectl create namespace flux-system
 gpg --export-secret-keys --armor "9DC11809DD7D102342B09932C40921F65EFEF866" | kubectl create secret generic sops-gpg --namespace=flux-system --from-file=sops.asc=/dev/stdin
-/usr/local/bin/flux bootstrap github \
-          --owner=ZeroNull7 \
-          --repository=home-k8s \
-          --path=cluster \
-	  --branch=main \
-          --personal
+
+flux create source git home-k8s \
+    --url=ssh://git@github.com/ZeroNull7/home-k8s.git \
+    --branch=main \
+    --interval=1m
+
+flux create kustomization base \
+        --source=GitRepository/home-k8s \
+        --path="./overlays/prod/base/sources" \
+        --prune=true \
+        --interval=5m
+
+
+flux create kustomization infra \
+      --source=GitRepository/home-k8s \
+      --path="./overlays/prod/infra/sources" \
+      --prune=true \
+      --interval=5m
